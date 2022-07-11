3d4ef6796d19ae2a9d8929d159716d6acb3c9b59
dunghenry <trandungksnb6930082001@gmail.com>
2022-07-11 14:40:50 +0700

    done

--- src/controllers/getDataController.js
+++ src/controllers/getDataController.js
@@ -12,7 +12,7 @@ const getDataController = {
             $(".portal", htmls).each(function () {
                 const name = $(this).find("a").attr("title");
                 const link = $(this).find("a").attr("href");
-                const url = `http://localhost:4000/v1${link.split("/wiki")[1]}`
+                const url = `http://localhost:4000/api/v1${link.split("/wiki")[1]}`
                 const image = $(this).find("a>img").attr("data-src");
                 const item = { name, url, image }
                 data.push(item);
@@ -21,6 +21,50 @@ const getDataController = {
                 return res.status(200).json(data.splice(0, limit));
             }
             return res.status(200).json(data);
+        } catch (error) {
+            await logEvents(error.message, module.filename);
+            return res.status(500).json(error);
+        }
+    },
+    getItem: async (req, res) => {
+        try {
+            const character = req.params.character;
+            const titles = [];
+            const values = [];
+            const obj = {};
+            const data = [];
+            const galleries = [];
+            const response = await axios.get(`https://kimetsu-no-yaiba.fandom.com/wiki/${character}`);
+            const htmls = response.data;
+            const $ = cheerio.load(htmls);
+            $(".wikia-gallery-item").each(function (){
+                const gallery = $(this).find("a > img").attr("data-src")
+                galleries.push(gallery);
+            })
+            $("aside", htmls).each(function () {
+                const image = $(this).find("img").attr("src");
+                let urlImage;
+                if(image){
+                    urlImage = image.slice(0, image.indexOf(".png") + 4)
+                }
+                $(this).find("section > div > h3").each(function () {
+                    titles.push($(this).text());
+                })
+                $(this).find("section > div > .pi-data-value").each(function () {
+                    values.push($(this).text());
+                })
+                for (var i = 0; i < titles.length; i++) {
+                    obj[titles[i].toLowerCase()] = values[i];
+                }
+                if(urlImage){
+                    data.push({
+                        name: character.replace("_", " "),
+                        image: urlImage,
+                        thumbnail: obj
+                    });
+                }
+            })
+            return res.status(200).json({...data[0], galleries});
         } catch (error) {
             await logEvents(error.message, module.filename);
             return res.status(500).json(error);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 const getDataController = require('../controllers/getDataController')
 const express = require('express');
 const router = express.Router();
-
 router.get('/', getDataController.getListData);
-
+router.get('/:character', getDataController.getItem);
 function route(app){
     app.use('/api/v1', router)
 }
-
 module.exports = route;
